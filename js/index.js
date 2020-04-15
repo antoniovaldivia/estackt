b4423464a896fe8802031967483aa151e92ed4a8
Antonio Valdivia <antoniovaldivia@gmail.com>
2020-04-15 16:09:19 -0400

    Version 1.0 validated

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+/* hi Rocco */
 function playSound(drums) {
-    document.getElementById(drums).play();
-  };
-  function playSound(bass) {
-    document.getElementById(bass).play();
-  };function playSound(synths) {
-    document.getElementById(synths).play();
-  };function playSound(drums) {
-    document.getElementById(drums).play();
-  };
+  document.getElementById(drums).play();
+};
+function playSound(bass) {
+  document.getElementById(bass).play();
+}; function playSound(synths) {
+  document.getElementById(synths).play();
+}; function playSound(melodies) {
+  document.getElementById(melodies).play();
+};
